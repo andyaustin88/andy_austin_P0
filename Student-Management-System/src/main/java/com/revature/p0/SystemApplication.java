f6e44093c8eec8515bf1c6cbf9a5b8e6fa526d60
Andy Austin <andyaustin88@Andys-MacBook-Pro.local>
2020-05-13 08:51:39 -0400

    Password Added

--- Student-Management-System/src/main/java/com/revature/p0/SystemApplication.java
+++ Student-Management-System/src/main/java/com/revature/p0/SystemApplication.java
@@ -1,15 +1,40 @@
 package com.revature.p0;
 
+import java.util.Scanner;
+
 public class SystemApplication {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		String username;
+		String password;
+		do {
+		
+		
+		System.out.print("Enter your username: ");
+		username = input.nextLine();
+		
+		
+		System.out.print("Enter your password: ");
+		password = input.nextLine();
+		
+		if (username.equals("andyaustin88") && (password.equals("Tigger"))) {
+			System.out.println("Welcome to Student Management System!");
+		}else {
+			System.out.println("Access Denied: Incorrect Username or password.");
+			
+		}
+		} 
+		while (!username.equals("andyaustin88") && !password.equals("Tigger"));	
 		
 		Student stu1 = new Student();
 		stu1.enroll();
 		stu1.payTuition();
 		System.out.println(stu1.showInfo());
 		
-		
+		input.close();
 	
 		// Create some sign in feature for authentication. 
 // arrange student files. What classes will they be enrolled in? 
---
+++ Student-Management-System/src/main/java/com/revature/p0/Username.java
@@ -0,0 +1,27 @@
+package com.revature.p0;
+import java.util.Scanner;
+public class Username {
+		
+		
+			
+			
+		/*	
+			String username;
+			String password;
+			
+			System.out.print("Enter your username: ");
+			username = input.nextLine();
+			
+			
+			System.out.print("Enter your password: ");
+			password = input.nextLine();
+			
+			if (username.equals("andyaustin88") && (password.equals("Tigger"))) {
+				System.out.println("Welcome to Student Management System!");
+			}else {
+				System.out.println("Access Denied: Incorrect Username or password.");
+			}
+		}
+}
+*/
+}
